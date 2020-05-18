f7910003bbb4b7074e6e14e1c0642db7e0cd2c53
Taylor Lafrinere <taylor@mytipreport.org>
2020-05-18 14:33:04 -0400

    Proto

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Strava.Activities;
+using Strava.Authentication;
+using Strava.Clients;
+using Strava.Clubs;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            StaticAuthentication auth = new StaticAuthentication("1208e775c37887b243769cd0222992a1bed60727");
+            StravaClient client = new StravaClient(auth);
+
+            Club club = client.Clubs.GetClub("661551");
+            List<ActivitySummary> activities = client.Clubs.GetLatestClubActivities(club.Id.ToString());
+        }
+    }
+}
